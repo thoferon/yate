528f6164100aae2198eece62fb180e81e4ec18d3
tho.feron <tho.feron@gmail.com>
2015-08-19 06:49:31 +0000

    Helper to parse and render in one function call

--- src/Text/Yate.hs
+++ src/Text/Yate.hs
@@ -3,7 +3,19 @@ module Text.Yate
   , YateValue(..)
   , ToYate(..)
   , parseTemplate
+  , renderTemplate
+  , renderTextTemplate
   ) where
 
-import Text.Yate.Parser
-import Text.Yate.Types
+import qualified Data.Text      as T
+import qualified Data.Text.Lazy as TL
+
+import           Text.Yate.Engine
+import           Text.Yate.Parser
+import           Text.Yate.Types
+
+-- | Parse and render a template in one function call
+renderTextTemplate :: ToYate a => T.Text -> a -> Either String TL.Text
+renderTextTemplate content dat = do
+  tpl <- parseTemplate content
+  renderTemplate tpl $ toYate dat
