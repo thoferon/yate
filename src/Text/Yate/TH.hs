ec9cddb19c247a48a06a2c5e7ce63fa265093269
tho.feron <tho.feron@gmail.com>
2015-08-19 08:49:30 +0000

    Template Haskell

---
+++ src/Text/Yate/TH.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Text.Yate.TH
+  ( tpl
+  , tplf
+  , loadTemplate
+  , loadTemplateFile
+  ) where
+
+import qualified Data.Text      as T
+import qualified Data.Text.Lazy as TL
+
+import           Language.Haskell.TH
+import           Language.Haskell.TH.Quote
+
+import           Text.Yate.Parser
+import           Text.Yate.Types
+
+tpl :: QuasiQuoter
+tpl = QuasiQuoter
+  { quoteExp  = loadTemplate
+  , quotePat  = error "The tpl quasiquoter is only for expressions"
+  , quoteType = error "The tpl quasiquoter is only for expressions"
+  , quoteDec  = error "The tpl quasiquoter is only for expressions"
+  }
+
+tplf :: QuasiQuoter
+tplf = quoteFile tpl
+
+loadTemplate :: String -> Q Exp
+loadTemplate str = case parseTemplate $ T.pack str of
+  Left  err -> fail $ "Failed to parse template: " ++ err
+  -- We don't care about the type argument here, it won't stay anyway
+  Right tpl -> templateToExp (tpl :: Template ())
+
+loadTemplateFile :: FilePath -> Q Exp
+loadTemplateFile path = do
+  content <- runIO $ readFile path
+  loadTemplate content
+
+templateToExp :: Template a -> Q Exp
+templateToExp tpl = case tpl of
+  Content  txt  -> [| Content $(litE $ StringL $ TL.unpack txt) |]
+  Variable path -> [| Variable $(pathToExp path) |]
+  If path yes no ->
+    [| If $(pathToExp path) $(templateToExp yes) $(templateToExp no) |]
+  For name path block ->
+    [| For $(litE $ StringL $ T.unpack name) $(pathToExp path)
+           $(templateToExp block) |]
+  In path block -> [| In $(pathToExp path) $(templateToExp block) |]
+  Parts parts -> [| Parts $(listE $ map templateToExp parts) |]
+
+pathToExp :: Path -> Q Exp
+pathToExp path = case path of
+    AbsolutePath names -> [| AbsolutePath $(mkNameList names) |]
+    RelativePath names -> [| RelativePath $(mkNameList names) |]
+  where
+    mkNameList :: [T.Text] -> Q Exp
+    mkNameList names = listE $ map (litE . StringL . T.unpack) names
---
+++ tests/Text/Yate/THSpec.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Text.Yate.THSpec where
+
+import qualified Data.HashMap.Strict as M
+
+import           Test.Hspec
+
+import           Text.Yate.Engine
+import           Text.Yate.TH
+import           Text.Yate.Types
+
+template :: Template YateValue
+template = [tpl|Hello, {%= name %}!|]
+
+spec :: Spec
+spec = describe "Template Haskell" $ do
+  it "parses the template properly" $ do
+    let dat = Object $ M.fromList [("name", String "Bob")]
+    renderTemplate template dat `shouldBe` Right "Hello, Bob!"
